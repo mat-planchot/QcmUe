514a95bde067c5eea50691b47ffb81088401c9a3
konki76 <mat_planchot@hotmail.fr>
2019-04-17 13:03:41 +0200

    Image DrawerNavigator

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
-import SafeViewAndroid from "./Components/SafeViewAndroid"
-import { SafeAreaView } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
-      <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
         <Navigation/>
-      </SafeAreaView>
     )
   }
 }
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,18 +1,20 @@
-import { createAppContainer, createStackNavigator, createDrawerNavigator } from 'react-navigation'
+import React from 'react'
+import { createAppContainer, DrawerItems, createDrawerNavigator } from 'react-navigation'
 import Qcm from '../Components/Qcm'
 import QcmFavorites from '../Components/QcmFavorites'
+import SafeViewAndroid from "../Components/SafeViewAndroid"
+import { View, Image, SafeAreaView } from "react-native";
 
-const QcmStackNavigator = createStackNavigator({
-  Qcm: {
-    screen: Qcm,
-    navigationOptions: {
-      title: 'Qcm'
-    }
-  },
-  QcmFavorites: {
-    screen: QcmFavorites
-  },
-})
+const CustomDrawer = (props) => (
+  <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+    <View style={{height: 150, alignItems: 'center', justifyContent: 'center'}}>
+      <Image
+        source={require('../img/Livre_ouvert_caducee.png')}
+      />
+    </View>
+    <DrawerItems {...props} />
+  </SafeAreaView>
+)
 
 const QcmDrawerNavigator = createDrawerNavigator({
   Qcm: {
@@ -21,6 +23,8 @@ const QcmDrawerNavigator = createDrawerNavigator({
   QcmFavorites: {
     screen: QcmFavorites
   },
+}, {
+  contentComponent: CustomDrawer
 })
 
 export default createAppContainer(QcmDrawerNavigator)
